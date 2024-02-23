39dbb84d6fa49cd490936e0659bab00c896f98e5
vicok2003 <vicok2003@gmail.com>
2024-02-23 20:33:30 +0100

    adding responsiveness

--- index.js
+++ index.js
@@ -12,3 +12,37 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+//navmobile
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.body.style.backgroundColor = "white";
+}
+
+//sidebar on scroll
+const sidenav = document.querySelector('.sidenav');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    sidenav.classList.add('scrolled');
+  } else {
+    sidenav.classList.remove('scrolled');
+  }
+});
+
+//swiper.js
+// Initialize Swiper
+var swiper = new swiper('.swiper-container', {
+  direction: 'vertical',
+            loop: true,
+            autoplay: {
+                delay: 5000,
+            },
+});
